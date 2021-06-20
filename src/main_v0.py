cd7a54718320d33e9e555e0c321261fde456e783
Jeffrey Chen <jeffrey.40408@gmail.com>
2021-06-20 23:32:55 +0800

    updates

--- src/main_v0.py
+++ src/main_v0.py
@@ -39,13 +39,13 @@ K = np.array([[982.16820326, 0., 534.53760602],
                 [0., 0., 1.]])
 
 
-# object_dict = {'fox': ['../3d_objects/fox/fox.obj', '../3d_objects/fox/texture.png'],
-#                'basketball-wilson': ['../3d_objects/basketball-wilson/basketball-wilson.obj', '../3d_objects/basketball-wilson/basketball-wilson.jpg'],
-#                'basketball': ['../3d_objects/basketball/basketball.obj', '../3d_objects/basketball/basketball.jpg'],
-#                'basketball-molten': ['../3d_objects/basketball-molten/molten.obj', '../3d_objects/basketball-molten/molten.jpg'],
-#                'board': ['../3d_objects/board/board.obj', '../3d_objects/board/board.jpg']}
+object_dict = {'fox': ['../3d_objects/fox/fox.obj', '../3d_objects/fox/texture.png'],
+               'basketball-wilson': ['../3d_objects/basketball-wilson/basketball-wilson.obj', '../3d_objects/basketball-wilson/basketball-wilson.jpg'],
+               'basketball': ['../3d_objects/basketball/basketball.obj', '../3d_objects/basketball/basketball.jpg'],
+               'basketball-molten': ['../3d_objects/basketball-molten/molten.obj', '../3d_objects/basketball-molten/molten.jpg'],
+               'board': ['../3d_objects/board/board.obj', '../3d_objects/board/board.jpg']}
 
-# obj = ThreeDimObject(object_dict['basketball-wilson'][0], object_dict['basketball-wilson'][1])
+obj = ThreeDimObject(object_dict['basketball-wilson'][0], object_dict['basketball-wilson'][1])
 
 def main():
     INVERSE_MATRIX = np.array([[ 1.0, 1.0, 1.0, 1.0],
@@ -62,47 +62,47 @@ def main():
         
         
         dst_pts, ids = findArucoMarkers(image)
-        if ids is not None and dst_pts is not None: 
-            rvecs, tvecs, _objpoints = aruco.estimatePoseSingleMarkers(dst_pts[0], 0.6, K)
-            #build view matrix
-            # board = aruco.GridBoard_create(6,8,0.05,0.01,aruco_dict)
-            # corners, ids, rejectedImgPoints,rec_idx = aruco.refineDetectedMarkers(gray,board,corners,ids,rejectedImgPoints)
-            # ret,rvecs,tvecs = aruco.estimatePoseBoard(corners,ids,board,self.cam_matrix,self.dist_coefs)
-            rmtx = cv2.Rodrigues(rvecs)[0]
+        # if ids is not None and dst_pts is not None: 
+        #     rvecs, tvecs, _objpoints = aruco.estimatePoseSingleMarkers(dst_pts[0], 0.6, K)
+        #     #build view matrix
+        #     # board = aruco.GridBoard_create(6,8,0.05,0.01,aruco_dict)
+        #     # corners, ids, rejectedImgPoints,rec_idx = aruco.refineDetectedMarkers(gray,board,corners,ids,rejectedImgPoints)
+        #     # ret,rvecs,tvecs = aruco.estimatePoseBoard(corners,ids,board,self.cam_matrix,self.dist_coefs)
+        #     rmtx = cv2.Rodrigues(rvecs)[0]
 
-            view_matrix = np.array([[rmtx[0][0],rmtx[0][1],rmtx[0][2],tvecs[0][0][0]],
-                                    [rmtx[1][0],rmtx[1][1],rmtx[1][2],tvecs[0][0][1]],
-                                    [rmtx[2][0],rmtx[2][1],rmtx[2][2],tvecs[0][0][2]],
-                                    [0.0       ,0.0       ,0.0       ,1.0    ]])
+        #     view_matrix = np.array([[rmtx[0][0],rmtx[0][1],rmtx[0][2],tvecs[0][0][0]],
+        #                             [rmtx[1][0],rmtx[1][1],rmtx[1][2],tvecs[0][0][1]],
+        #                             [rmtx[2][0],rmtx[2][1],rmtx[2][2],tvecs[0][0][2]],
+        #                             [0.0       ,0.0       ,0.0       ,1.0    ]])
 
             # view_matrix = np.array([[rmtx[0][0],rmtx[0][1],rmtx[0][2],tvecs[0]],
             #                         [rmtx[1][0],rmtx[1][1],rmtx[1][2],tvecs[1]],
             #                         [rmtx[2][0],rmtx[2][1],rmtx[2][2],tvecs[2]],
             #                         [0.0       ,0.0       ,0.0       ,1.0    ]])
 
-            view_matrix = view_matrix * INVERSE_MATRIX
+            # view_matrix = view_matrix * INVERSE_MATRIX
 
-            view_matrix = np.transpose(view_matrix)
+            # view_matrix = np.transpose(view_matrix)
 
             # load view matrix and draw shape
-            glPushMatrix()
-            glLoadMatrixd(view_matrix)
+            # glPushMatrix()
+            # glLoadMatrixd(view_matrix)
 
-            glCallList(self.bball.gl_list)
+            # glCallList(self.bball.gl_list)
 
-            glPopMatrix()
+            # glPopMatrix()
         
         # if there is something, find homography
-        # if ids:
-        #     # Calculate homography
-        #     marker = cv2.imread('../markers/marker.png')
-        #     marker = cv2.cvtColor(marker, cv2.COLOR_BGR2GRAY)
-        #     H = calHomography(marker, dst_pts)
+        if ids:
+            # Calculate homography
+            marker = cv2.imread('../markers/marker.png')
+            marker = cv2.cvtColor(marker, cv2.COLOR_BGR2GRAY)
+            H = calHomography(marker, dst_pts)
 
-        #     R_T = get_extended_RT(K, H)
-        #     transformation = K.dot(R_T) 
+            R_T = get_extended_RT(K, H)
+            transformation = K.dot(R_T) 
             
-        #     image = augment(image, obj, transformation, marker, True)
+            image = augment(image, obj, transformation, marker, True)
             # image = np.flip(augment(image, obj, transformation, marker, True), axis = 1)
 
         # image, multi_hand_landmarks = detectHandPose(image)
